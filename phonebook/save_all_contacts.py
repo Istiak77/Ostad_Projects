7a5e51f97e0ed57f95e2d0d1f068cc8f20fd4cdf
Istiak <ahmedpranto666@gmail.com>
2024-12-04 23:44:03 +0600

    Organized files into phonebook folder

---
+++ phonebook/save_all_contacts.py
@@ -0,0 +1,35 @@
+import csv
+import os 
+
+
+def save_all_contacts(all_contacts):
+    project_folder = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(project_folder, "all_contacts.csv")
+
+    with open(file_path, "w") as fp:
+        fp.write("Name,Phone Number,Email,Address\n")
+        for contact in all_contacts:
+            line = f"{contact['name']},{contact['number']},{contact['email']},{contact['address']}\n"
+            fp.write(line)
+
+
+
+def load_contacts():
+    contacts = []
+
+    files = os.listdir() 
+    if "all_contacts.csv" in files:
+        with open("all_contacts.csv", "r") as fp:
+            reader = csv.DictReader(fp)
+            for row in reader:
+                contact = {
+                    "name": row["Name"],
+                    "number": row["Phone Number"],
+                    "email": row["Email"],
+                    "address": row["Address"]
+                }
+                contacts.append(contact)
+    else:
+        print("\nNo contacts found.\n")
+
+    return contacts
