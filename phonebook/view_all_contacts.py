1d1216498f5b15040515671180c6a5a9919618e9
Istiak <ahmedpranto666@gmail.com>
2025-01-17 23:44:28 +0600

    deleted main branch

--- phonebook/view_all_contacts.py
+++
@@ -1,7 +0,0 @@
-def view_all_contacts(all_contacts):
-    if all_contacts !=[]:
-        for contact in all_contacts:
-            print(f"\nName: {contact['name']}\nNumber: {contact['number']}\nEmail: {contact['email']}\nAddress: {contact['address']}\n")
-
-    else:
-        print("\nNo contacts found\n")
